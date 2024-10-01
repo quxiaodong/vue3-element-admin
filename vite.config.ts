10447cf95be673f9581a0bb325f4693d7256b442
quxiaodong <quxiaodong1990@gmail.com>
2024-10-01 14:37:41 +0800

    feat: 配置路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  },
   plugins: [vue()]
 })
