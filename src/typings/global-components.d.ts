140a32bc21049838521d463c1f3447714d02e325
quxiaodong <quxiaodong1990@gmail.com>
2024-10-02 19:03:31 +0800

    feat: 添加通用弹窗

--- src/typings/global-components.d.ts
+++ src/typings/global-components.d.ts
@@ -1,9 +1,11 @@
 import CommonDark from '@/components/common-dark/Index.vue'
+import CommonDialog from '@/components/common-dialog/Index.vue'
 import CommonLocale from '@/components/common-locale/Index.vue'
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     CommonDark: typeof CommonDark
     CommonLocale: typeof CommonLocale
+    CommonDialog: typeof CommonDialog
   }
 }
