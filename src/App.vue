a1cb03af21ca3789f6d396f2c0a810b4c8293542
quxiaodong <quxiaodong1990@gmail.com>
2024-10-03 16:06:24 +0800

    feat: 添加第三方库

--- src/App.vue
+++ src/App.vue
@@ -1,2 +1,33 @@
 <template>
+  <el-config-provider :locale>
+    <el-watermark :content :font :z-index="9999">
+      <router-view />
+    </el-watermark>
+  </el-config-provider>
 </template>
+
+<script setup lang="ts">
+import { useColorSchemaStore } from '@/stores/color-schema'
+import { useLocaleStore } from '@/stores/locale'
+import { WatermarkFontType } from 'element-plus'
+import * as locales from 'element-plus/es/locale/index.mjs'
+import { computed, ComputedRef } from 'vue'
+
+const localeStore = useLocaleStore()
+const colorSchemaStore = useColorSchemaStore()
+
+const content = ['development', 'mock'].includes(import.meta.env.MODE)
+  ? '内容仅供测试和展示'
+  : ''
+
+const locale = computed(
+  () => locales[localeStore.locale?.code as keyof typeof locales]
+)
+
+const font: ComputedRef<WatermarkFontType> = computed(() => ({
+  fontSize: 16,
+  color: colorSchemaStore.isDark
+    ? 'rgba(255, 255, 255, 0.1)'
+    : 'rgba(0, 0, 0, 0.1)'
+}))
+</script>
---
+++ src/typings/vue-router.d.ts
@@ -0,0 +1,10 @@
+import 'vue-router'
+
+declare module 'vue-router' {
+  interface RouteMeta {
+    breadcrumb: string
+    i18n?: string
+    icon?: string
+    hideInMenu?: boolean
+  }
+}
