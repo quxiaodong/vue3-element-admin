203aa1afe6e7585a80b71644ac69ceba43606817
quxiaodong <quxiaodong1990@gmail.com>
2024-10-12 11:33:01 +0800

    feat: 添加通用搜索

--- src/typings/global-components.d.ts
+++ src/typings/global-components.d.ts
@@ -2,6 +2,7 @@ import CommonDark from '@/components/common-dark/Index.vue'
 import CommonDialog from '@/components/common-dialog/Index.vue'
 import CommonForm from '@/components/common-form/Index.vue'
 import CommonLocale from '@/components/common-locale/Index.vue'
+import CommonSearch from '@/components/common-search/Index.vue'
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
@@ -9,5 +10,6 @@ declare module '@vue/runtime-core' {
     CommonLocale: typeof CommonLocale
     CommonDialog: typeof CommonDialog
     CommonForm: typeof CommonForm
+    CommonSearch: typeof CommonSearch
   }
 }
