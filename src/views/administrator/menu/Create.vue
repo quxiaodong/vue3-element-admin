4986b600156e5d72e3fe8330d76c2bbd0d9dd68a
quxiaodong <quxiaodong1990@gmail.com>
2024-10-18 13:59:47 +0800

    feat: 菜单管理页面

---
+++ src/views/administrator/menu/Create.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <el-button type="primary" icon="Plus" @click="click">
+      {{ $t('common.create') }}
+    </el-button>
+    <common-dialog v-model="show" :title="$t('common.create')">
+      <Form v-model="model" :loading @submit="submit" />
+    </common-dialog>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { createMenu, CreateMenuInput } from '@/apis/administrator/menu'
+import { ref } from 'vue'
+import Form from './Form.vue'
+
+type Emits = {
+  (e: 'refetch'): void
+}
+
+const emits = defineEmits<Emits>()
+
+const initValue: () => CreateMenuInput = () => ({
+  name: '',
+  path: '',
+  status: 0
+})
+
+const model = ref<CreateMenuInput>(initValue())
+
+const show = ref(false)
+
+const { execute, loading } = createMenu()
+
+const click = () => {
+  show.value = true
+  model.value = initValue()
+}
+
+const submit = () =>
+  execute(model.value).then(() => {
+    show.value = false
+    emits('refetch')
+  })
+</script>
