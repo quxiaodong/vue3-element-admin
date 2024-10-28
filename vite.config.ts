88559cdf030eb3f64c22c0624163cba595b53c5c
quxiaodong <quxiaodong1990@gmail.com>
2024-10-28 20:54:43 +0800

    feat: 配置环境变量

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  envDir: resolve(__dirname, './env'),
   resolve: {
     alias: {
       '@': resolve(__dirname, './src')
