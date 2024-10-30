9c68dbdbe107779df611fc5b7336c844245bb8ca
quxiaodong <quxiaodong1990@gmail.com>
2024-10-30 11:19:49 +0800

    feat: 通用暗黑模式

---
+++ src/components/common-dark/Index.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-dropdown @command="toggleMode">
+    <div class="placeholder">
+      <el-icon :size="18">
+        <component :is="mode?.icon" />
+      </el-icon>
+    </div>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item
+          v-for="{ code, icon, stem } in modes"
+          :key="code"
+          :command="code"
+          :class="{ 'dropdown-item-active': code === mode?.code }"
+          @click="clickItem"
+        >
+          <el-icon>
+            <component :is="icon" />
+          </el-icon>
+          {{ $t(stem) }}
+        </el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+</template>
+
+<script setup lang="ts">
+import { clickItem, mode, modes, toggleMode } from './stores'
+</script>
